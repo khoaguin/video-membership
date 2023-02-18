34c1e93eb9587c6b41a106a4b012fad2a44a863c
khoaguin <dkn.work@protonmail.com>
2023-02-18 14:57:09 +0700

    on configuring AstraDB with FastAPI

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 
+## Install requirements
+You need `python 3.10.8`
+
+
+```
+python -m venv .venv  # create the virtual environment
+source .venv/bin/activate  # activate the virtual environment
+pip install --upgrade pip  # update pip
+pip install -r requirements.txt  # install the required packages
+```
 
 ## Run the server
 ```
---
+++ app/db.py
@@ -0,0 +1,21 @@
+import pathlib
+
+from cassandra.auth import PlainTextAuthProvider
+from cassandra.cluster import Cluster
+from cassandra.cqlengine import connection
+
+BASE_DIR = pathlib.Path(__file__).resolve()
+
+ASTRADB_CONNECT_BUNDLE = BASE_DIR / "unencypted/astradb_connect.zip"
+ASTRADB_CLIENT_ID = "<<CLIENT ID>>"
+ASTRADB_CLIENT_SECRET = "<<CLIENT SECRET>>"
+
+
+def get_session():
+    cloud_config = {"secure_connect_bundle": ASTRADB_CONNECT_BUNDLE}
+    auth_provider = PlainTextAuthProvider(ASTRADB_CLIENT_ID, ASTRADB_CLIENT_SECRET)
+    cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+    session = cluster.connect()
+    connection.register_connection(str(session), session=session)
+    connection.set_default_connection(str(session))
+    return session
