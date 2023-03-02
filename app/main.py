7d7d4978ef811fab94bc2b6212c5113fcd439fc2
khoaguin <dkn.work@protonmail.com>
2023-03-02 22:20:35 +0700

    done Pydantic Validate Data or Error

--- app/main.py
+++ app/main.py
@@ -1,12 +1,24 @@
-from cassandra.cqlengine.management import sync_table
-from fastapi import FastAPI
+import json
+import pathlib
 
-from . import config, db
+from cassandra.cqlengine.management import sync_table
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from pydantic.error_wrappers import ValidationError
+
+from . import config, db, utils
 from .users.models import User
+from .users.schemas import UserLoginSchema, UserSignupSchema
+
+BASE_DIR = pathlib.Path(__file__).resolve().parent
+TEMPLATE_DIR = BASE_DIR / "templates"
 
 app = FastAPI()
-# the global session. We do not want to initialize
-# a new session every time
+templates = Jinja2Templates(directory=TEMPLATE_DIR)
+
+# the global database session. We do not want
+# to initialize a new session every time
 DB_SESSION = None
 
 
@@ -19,11 +31,73 @@ def on_startup():
     sync_table(User)
 
 
-@app.get("/")
-def homepage():
-    return {
-        "Hello": "World",
-    }  # json data -> REST API
+@app.get("/", response_class=HTMLResponse)
+def homepage(request: Request):
+    context = {"request": request, "abc": 123}
+    return templates.TemplateResponse("home.html", context)
+
+
+@app.get("/login", response_class=HTMLResponse)
+def login_get_view(request: Request):
+    return templates.TemplateResponse(
+        "auth/login.html",
+        {
+            "request": request,
+        },
+    )
+
+
+@app.post("/login", response_class=HTMLResponse)
+def login_post_view(
+    request: Request, email: str = Form(...), password: str = Form(...)
+):
+    raw_data = {
+        "email": email,
+        "password": password,
+    }
+    data, errors = utils.valid_schema_data_or_error(raw_data, UserLoginSchema)
+    print(data["password"].get_secret_value())
+    return templates.TemplateResponse(
+        "auth/login.html",
+        {
+            "request": request,
+            "data": data,
+            "errors": errors,
+        },
+    )
+
+
+@app.get("/signup", response_class=HTMLResponse)
+def signup_get_view(request: Request):
+    return templates.TemplateResponse(
+        "auth/signup.html",
+        {
+            "request": request,
+        },
+    )
+
+
+@app.post("/signup", response_class=HTMLResponse)
+def signup_post_view(
+    request: Request,
+    email: str = Form(...),
+    password: str = Form(...),
+    password_confirm: str = Form(...),
+):
+    raw_data = {
+        "email": email,
+        "password": password,
+        "password_confirm": password_confirm,
+    }
+    data, errors = utils.valid_schema_data_or_error(raw_data, UserSignupSchema)
+    return templates.TemplateResponse(
+        "auth/signup.html",
+        {
+            "request": request,
+            "data": data,
+            "errors": errors,
+        },
+    )
 
 
 @app.get("/users")
